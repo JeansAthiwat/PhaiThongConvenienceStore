537bf280479de8642810b1c3a94f7debcf1f6d15
JeansAthiwat <athiwat5146@gmail.com>
2023-08-28 23:29:29 +0700

    todo: before move to logic package

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,10 @@ package application;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
 //TODO
     }
 }
--- src/store/Store.java
+++ src/store/Store.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 
 public class Store {
     //TODO
-    private final ArrayList<Item> stocks = new ArrayList<Item>();
+    private final ArrayList<Item> stock = new ArrayList<Item>();
 
-    private final ArrayList<BasicMember> members  = new ArrayList<BasicMember>();
+    private final ArrayList<BasicMember> members = new ArrayList<BasicMember>();
 
     private int storeMoney;
 
@@ -16,7 +16,7 @@ public class Store {
 
 
     public static Store getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new Store(100);
         }
         return instance;
@@ -24,15 +24,39 @@ public class Store {
 
     public Store(int storeMoney) {
         //TODO : add each type of customer
-        this.getMembers().add(new BasicMember("Jeans",512546));
-        this.getMembers().add(new BasicMember("Faro",555445));
-        this.getMembers().add(new BasicMember("Peet",123456));
-        this.getMembers().add(new BasicMember("Beer",77777));
-        this.storeMoney = storeMoney;
+        this.getMembers().add(new BasicMember("Jeans", 512546));
+        this.getMembers().add(new BasicMember("Faro", 555445));
+        this.getMembers().add(new BasicMember("Peet", 123456));
+        this.getMembers().add(new BasicMember("Beer", 77777));
+
+        this.getStock().add(new Item("Jelly Beans",100, 50));
+        this.getStock().add(new Item("Painkiller",200, 5));
+        this.getStock().add(new Item("Orange",150, 10));
+        this.getStock().add(new Item("Coconut Milk",300, 30));
+        this.setStoreMoney(storeMoney);
     }
 
-    public ArrayList<Item> getStocks() {
-        return stocks;
+    public void checkOut(BasicMember member) {
+        //TODO:
+
+    }
+
+    public void addItemToShoppingCart(BasicMember member, Item item) {
+        this.getStock().remove(item);
+        member.getShoppingCart().add(item);
+    }
+
+    public boolean isInStock(Item OtherItem) {
+        for (Item item : this.getStock()) {
+            if (OtherItem == item) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<Item> getStock() {
+        return stock;
     }
 
     public ArrayList<BasicMember> getMembers() {
@@ -44,7 +68,7 @@ public class Store {
     }
 
     public void setStoreMoney(int storeMoney) {
-        if(storeMoney < 0){
+        if (storeMoney < 0) {
             storeMoney = 0;
         }
         this.storeMoney = storeMoney;
