4124fcca06e08633fb7a8425e1ef7c9d309c6988
JeansAthiwat <athiwat5146@gmail.com>
2023-08-31 20:29:05 +0700

    feat: StarvingStudentMember

--- src/logic/member/StarvingStudentMember.java
+++ src/logic/member/StarvingStudentMember.java
@@ -4,7 +4,7 @@ import logic.store.Store;
 
 public class StarvingStudentMember extends FundamentalMintMember {
     static final int MAX_LOAN = 1920;
-    int totalLoan;
+    int loan;
 
     public StarvingStudentMember(String name, int memberID, int point, int digitalMoney) {
         super(name, memberID, point, digitalMoney);
@@ -12,16 +12,29 @@ public class StarvingStudentMember extends FundamentalMintMember {
         this.discountPercent = 0.20;
     }
 
-    public void loanMoney(int amount) {
-       // Store store = Store.getInstance().getStoreMoney();
-
+    public boolean loanMoney(int amount) {
+        if (this.getLoan() + amount <= MAX_LOAN) {
+            Store store = Store.getInstance();
+            store.setStoreMoney(store.getStoreMoney() - amount);
+            this.setDigitalMoney(this.getDigitalMoney() + amount);
+            this.setLoan(this.getLoan() + amount);
+            return true;
+        }
+        return false;
     }
 
-    public int getTotalLoan() {
+    public void returnLoan(int amount) {
+        Store store = Store.getInstance();
+        store.setStoreMoney(store.getStoreMoney() + amount);
+        this.setDigitalMoney(this.getDigitalMoney() - amount);
+        this.setLoan(this.getLoan() - amount);
+    }
+
+    public int getLoan() {
         return loan;
     }
 
-    public void setTotalLoan(int loan) {
+    public void setLoan(int loan) {
         if (loan < 0) {
             loan = 0;
         }
